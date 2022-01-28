c1324e209d8af6a4c7c87c83cf534343916cdee6
jemin <jemin03120111@gmail.com>
2022-01-28 16:23:12 +0900

    first init

---
+++ src/test/java/study/githubactionsdeploy/GithubactionsdeployApplicationTests.java
@@ -0,0 +1,13 @@
+package study.githubactionsdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubactionsdeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
